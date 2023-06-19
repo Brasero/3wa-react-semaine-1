360006f6cb88ae0821a53b1b6fb5e0e8e5d7cb47
Brasero <brandonricci4@gmail.com>
2023-06-19 17:23:07 +0200

    correction du challenge chrono

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 import Counter from "./Counter.jsx";
 
+import Chrono from "./Chrono.jsx";
+
 function App() {
 
     return (
         <>
-            <Counter step={1} />
-            <Counter step={3} />
-            <Counter step={6} />
+            {
+                /*
+                <Counter step={1}/>
+                <Counter step={3} />
+                <Counter step={6} />
+             */
+            }
+
+            <Chrono step={1} />
+            <Chrono step={2} />
         </>
     )
 }
---
+++ src/Chrono.jsx
@@ -0,0 +1,63 @@
+import {useState, useEffect} from "react";
+
+function Chrono({step, limit = 20}) {
+
+    const [chrono, setChrono] = useState(0)
+    const [intervalId, setIntervalId] = useState(0)
+    const [disabled, setDisabled] = useState(false)
+    const [message, setMessage] = useState("")
+
+
+    const start = () => {
+        //sécurité
+        clearInterval(intervalId)
+        if (chrono >= limit) {
+            setMessage("La limite à été atteinte merci de remettre le chrono à 0")
+            return
+        }
+        const id = setInterval(() => {
+            setChrono(chrono => chrono + step)
+            console.log(chrono)
+        }, 1000)
+        setDisabled(true)
+        setIntervalId(id)
+    }
+
+    const stop = () => {
+        clearInterval(intervalId)
+        setDisabled(false)
+    }
+
+    const reset = () => {
+        stop()
+        setChrono(0)
+        setMessage('')
+    }
+
+    useEffect(() => {
+        if(chrono >= limit) {
+            stop()
+        }
+    }, [chrono])
+
+    return (
+        <div>
+            <div>{chrono}</div>
+            <div style={{
+                display: 'flex',
+                gap: '5px'
+            }} >
+                <button disabled={disabled} onClick={start}>Start (+{step})</button>
+                <button disabled={!disabled} onClick={stop}>Stop</button>
+                {
+                    chrono > 0 && <button onClick={reset}>Reset</button>
+                }
+            </div>
+            {
+                message !== '' && <span style={{color: "red"}}>{message}</span>
+            }
+        </div>
+    )
+}
+
+export default Chrono
