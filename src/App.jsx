32839d74ac010593cadf3ae24fcc672b3f786ecd
Brasero <brandonricci4@gmail.com>
2023-06-21 10:36:12 +0200

    cleaning project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
-import Counter from "./Counter.jsx";
 
 function App() {
 
     return (
         <>
-            <Counter step={1} />
-            <Counter step={3} />
-            <Counter step={6} />
         </>
     )
 }
