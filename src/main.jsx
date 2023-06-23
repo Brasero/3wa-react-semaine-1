dce1a6d487d31d194e12e85b7cba7ed53664fdbc
Brasero <brandonricci4@gmail.com>
2023-06-23 17:34:50 +0200

    proposition de solution pour l'exercice d'evaluation

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import OutgoingContextProvider from "./utils/context/OutgoingContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-        <App />
+        <OutgoingContextProvider>
+            <App />
+        </OutgoingContextProvider>
   </React.StrictMode>,
 )
